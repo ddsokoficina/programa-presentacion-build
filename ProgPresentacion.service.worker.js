daa46c81425e86d6453511b54214886a2d2e01c3
ddsokoficina <ddsokoficina@gmail.com>
2026-02-18 12:07:39 +0100

    downgrade emscripten

--- ProgPresentacion.service.worker.js
+++ ProgPresentacion.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771410122|4523211240';
+const CACHE_VERSION = '1771412842|7243303092';
 /** @type {string} */
 const CACHE_PREFIX = 'Grupo Creando-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
